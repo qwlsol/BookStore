a36d7dfc18c5c87a2f6ac98f9b7828c029b7d522
st403 <st403@WS403-04>
2026-04-11 09:13:30 +0300

    была доделана модель Автор, были переведены страницы на bootstrap карточки, была сделана темная тема(переключение со светлой на темную), а так же была реализована проверка обязательных полей с выводом ошибки пользователю для книг и покупателей

--- Pages/Books/Edit.cshtml.cs
+++ Pages/Books/Edit.cshtml.cs
@@ -3,6 +3,7 @@ using BookStore.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Pages.Books
 {
@@ -18,15 +19,20 @@ namespace BookStore.Pages.Books
         [BindProperty]
         public Book? Book { get; set; }
 
+        [BindProperty]
+        [Required(ErrorMessage = "Укажите автора")]
+        public string AuthorName { get; set; } = string.Empty;
         public IActionResult OnGet(int id)
         {
             Book = _context.Books
-                       .Where(c => c.Id == id)
-                       .Include(b => b.Author)
-                       .FirstOrDefault();
+                      .Where(c => c.Id == id)
+                      .Include(b => b.Author)
+                      .FirstOrDefault();
 
             if (Book == null)
                 return NotFound();
+
+            AuthorName = Book.Author?.Name ?? string.Empty;
 
             return Page();
         }
@@ -35,6 +41,17 @@ namespace BookStore.Pages.Books
         {
             if (!ModelState.IsValid)
                 return Page();
+
+            var author = _context.Authors.FirstOrDefault(a => a.Name == AuthorName);
+            if (author == null)
+            {
+                author = new Author { Name = AuthorName };
+                _context.Authors.Add(author);
+                _context.SaveChanges();
+            }
+
+            Book.AuthorID = author.Id;
+            Book.Author = author;
 
             _context.Books.Update(Book);
             _context.SaveChanges();
