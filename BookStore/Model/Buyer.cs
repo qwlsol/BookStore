075f3f4f39038758bb0ef0650b92227ab4fa027a
DmtrDv <dmitriybychkov75146@gmail.com>
2026-04-25 13:01:37 +0300

    были сделаны рабочие тесты

--- BookStore/Model/Buyer.cs
+++ BookStore/Model/Buyer.cs
@@ -6,9 +6,9 @@ namespace BookStore.Model
     {
         [Required(ErrorMessage = "Строка 'Фамилия' обязательна к заполнению")]
         public string LastName { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Строка 'Email' обязательна к заполнению")]
+        [EmailAddress(ErrorMessage = "Строка 'Email' обязательна к заполнению")]
         public string? Email { get; set; }
-        [Required(ErrorMessage = "Строка  'Телефон' обязательна к заполнению")]
+        [Phone(ErrorMessage = "Строка  'Телефон' обязательна к заполнению")]
         public string? Phone { get; set; }
         public List<Book>? Books { get; set; }
     }
---
+++ BookStoreTest/Pages/BooksIndexTests.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Pages.Books;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data;
+using BookStore.Model;
+
+namespace BookStore.Tests.Pages;
+
+public class BooksIndexTests
+{
+    [Fact]
+    public void IndexModel_OnGet_LoadsBooksFromDatabase()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new ApplicationDbContext(options);
+
+        context.Books.Add(new Book { Title = "Книга 1", Price = 100, Quantity = 5 });
+        context.Books.Add(new Book { Title = "Книга 2", Price = 200, Quantity = 3 });
+        context.SaveChanges();
+
+        var model = new IndexModel(context);
+        model.OnGet();
+
+        Assert.Equal(2, model.Books.Count);
+    }
+
+    [Fact]
+    public void IndexModel_Exists_AndHasBooksProperty()
+    {
+        var model = new IndexModel(null!);
+
+        Assert.NotNull(model);
+        Assert.IsType<IndexModel>(model);
+    }
+}
