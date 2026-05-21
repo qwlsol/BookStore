df524bef649d2ca582f23775897d7d3d1484bf56
guzer <guzer@DESKTOP-K1P33T2>
2026-05-21 15:54:11 +0300

    изменено перенаправление

--- BookStoreTest/obj/Debug/net9.0/BookStoreTest.AssemblyInfo.cs
+++ BookStoreTest/obj/Debug/net9.0/BookStoreTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe03d55f756aa9a82574136590a0001b0f8997c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ff0ce383fb2a4c418dfbf417b90f065d3aec724")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
